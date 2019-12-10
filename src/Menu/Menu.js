2e3e50ad58c248e80046c7cd61b2b590954e6dc0
Igor <igor.salesmarketing@gmail.com>
2019-12-10 15:19:56 +0300

    Изменил кнопку выхода

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components'
 import {connect} from "react-redux";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
 
 
 const Wrapper = styled.div`
@@ -8,6 +10,35 @@ const Wrapper = styled.div`
     flex-direction: row-reverse;
     position: fixed;
     width: 100%;
+`;
+
+const ExitIcon = styled(FontAwesomeIcon)`
+    color: #5264AE;
+    margin-left: 5px;
+    margin-top: 6px;
+    :hover {
+        color: #02122c;
+        cursor: pointer;
+    }
+`;
+
+const Exit = styled.div`
+    margin-top: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-right: 20px;
+`;
+
+const ExitSpan = styled.span`
+   
+    color: #5264AE;
+    font-size: 17px;
+    border-bottom: 1px solid transparent;
+    
+    :hover {
+        cursor: pointer;
+        border-bottom: 1px dashed #5264AE;
+    }
 `;
 
 const ExitButton = styled.button`
@@ -27,13 +58,16 @@ const ExitButton = styled.button`
         background: transparent;
         border: 2px solid #1F5C53;
         color: #1F5C53;
+        cursor: pointer;
     }
 `;
 
 const Menu = (props) => {
     return (
         <Wrapper>
-            <ExitButton onClick={props.handleExit} type={'submit'}>Выйти из приложения</ExitButton>
+            <Exit onClick={props.handleExit}>
+                <ExitSpan>Выход <ExitIcon icon={faSignOutAlt}/></ExitSpan>
+            </Exit>
         </Wrapper>
     )
 };
